088b6abb2306bbdb944a4d42763bc8a5191c0f6c
Marcosino4 <marcosino654@gmail.com>
2023-11-22 14:09:42 +0100

    Palas movement added

--- Assets/Scripts/PalaMovement.cs
+++ Assets/Scripts/PalaMovement.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class PalaMovement : MonoBehaviour
 {
     public  float speed = 0;
-    private void Awake()
-    {
-                
-    }
+    public KeyCode up;
+    public KeyCode down;
+    private Vector2 direccion;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-              
+              rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direccion = Vector2.zero;
+        if (Input.GetKey(up))
+        {
+            direccion = new Vector2(0, 1);
+        }else if (Input.GetKey(down))
+        {
+            direccion = new Vector2(0, -1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Update que se ejecuta cada más tiempo
+        rb.velocity = direccion * speed;    
     }
 }
